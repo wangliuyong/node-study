bdc94b32cf54613a1e8509f510b12968f22716b6
wly <1355498705@qq.com>
2019-12-28 15:49:25 +0800

    acc

---
+++ 2.node中的全局对象/node-global.js
@@ -0,0 +1,15 @@
+// 1.浏览器中js？
+/**
+ * 1.BOM 浏览器对象模型 （location/ history）
+ * 2.DOM 文档对象模型 （DOM的增删改查）
+ * 3.ES规范    ES5   ES6
+ */
+
+ // 2.node 中的js？
+ /**
+  * 1.没有DOM  && BOM
+  * 2.包含所有的es规范
+  * 3.没有了window，但是有了global的全局变量
+  */
+
+  console.log(global)  //  Buffer 
